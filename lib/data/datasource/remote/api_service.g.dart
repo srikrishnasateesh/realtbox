4c9129cf507f7b321e3e8a3926da153f9b0192a7
Sateesh <srikrishnasateesh@gmail.com>
2025-03-05 16:18:34 +0530

    base url changed to production

--- lib/data/datasource/remote/api_service.g.dart
+++ lib/data/datasource/remote/api_service.g.dart
@@ -14,7 +14,7 @@ class _ApiService implements ApiService {
     this.baseUrl,
     this.errorLogger,
   }) {
-    baseUrl ??= 'https://api.dev.propertybox.co.in/';
+    baseUrl ??= 'https://api.propertybox.co.in/';
   }
 
   final Dio _dio;
